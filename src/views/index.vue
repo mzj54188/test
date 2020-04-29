198c168dc6c912b41586c45abcb6a8d6742a0332
OMGJay <403560384@qq.com>
2020-04-29 12:14:47 +0800

    test

---
+++ src/views/index.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="page">
+        <el-button
+            :disabled="isDisabled"
+            @click="saveCli">
+            {{buttonName}}
+        </el-button>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return {
+            isDisabled:false,
+            second:null,
+            buttonName:'按钮'
+        }
+    },
+    methods:{
+        saveCli(){
+            const _this=this
+            _this.isDisabled = true;
+            const interval = window.setInterval(function() {
+              _this.buttonName = "(" + _this.second + "秒)后重新发送";
+              --_this.second;
+              if (_this.second < 0) {
+                _this.buttonName = "重新发送";
+                _this.second = 60;
+                _this.isDisabled = false;
+                window.clearInterval(interval);
+              }
+            }, 1000)
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
+
---
+++ src/views/pic.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="page">
+        <img src="../../public/img/timg.jpg" alt="">
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{}
+    },
+    methods:{
+    }
+}
+</script>
+<style lang="scss" scoped>
+.page{
+    width: 300px;
+    height: 300px;
+    border: #000 solid 1px;
+    margin: 50px auto;
+    overflow: hidden;
+    & img{
+        cursor: crosshair;
+        transition: all 0.6s ease 0s;
+    }
+    & img:hover{
+        transform: scale(1.4);
+    }
+}
+</style>
+
